d029e2677e1871f40d88665d73d1b1ddd36fc60c
Santiago Garcia <102694038+Santy1422@users.noreply.github.com>
2023-08-22 14:20:05 +0200

    update

--- nombre-de-tu-app/src/Components/dashboard/Dashboard.jsx
+++ nombre-de-tu-app/src/Components/dashboard/Dashboard.jsx
@@ -54,20 +54,19 @@ export const Dashboard = () => {
         const worksheet = workbook.Sheets[sheetName];
         const rows = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
-        const asinArray = rows.map(row => row[0].trim());
+        const asinArray = rows.map(row => row[0]?.trim());
         
         // Send the POST request similar to the ASIN search request
-        const response = await axios.post('https://amazon-scraper-api-production.up.railway.app/products', {
-          productIds: asinArray
-        });
+       const response = await axios.post('https://amazon-scraper-api-production.up.railway.app/products', {
+        productIds: asinArray
+      });
 
-        console.log('Response from server:', response.data);
-        setUploadedProducts(response.data);
-        setLoading(false);
-      };
-      fileReader.readAsArrayBuffer(selectedFile);
+      console.log('Response from server:', response.data);
+      setUploadedProducts(response.data);
+      setLoading(false);
+      }
     } catch (error) {
-      console.error('Error uploading products:', error);
+      console.error('Error fetching data:', error);
       setLoading(false);
     }
   };
@@ -112,40 +111,29 @@ export const Dashboard = () => {
     <>
       <Navbar />
       <div className="p-4 max-w-xl mx-auto">
-        <h2 className="text-xl font-semibold mb-4">Dashboard</h2>
+        <h2 className="text-xl font-semibold mb-4 text-white">Dashboard</h2>
         <div className="flex mb-4">
-          <button
-            onClick={() => setActiveTab("search")}
-            className={`mr-2 px-4 py-2 rounded ${
-              activeTab === "search" ? "bg-blue-500 text-white" : "bg-gray-300 text-gray-800"
-            }`}
-          >
-            Buscar por ASIN
-          </button>
-          <button
-            onClick={() => setActiveTab("import")}
-            className={`px-4 py-2 rounded ${
-              activeTab === "import" ? "bg-blue-500 text-white" : "bg-gray-300 text-gray-800"
-            }`}
-          >
-            Importar con Excel
-          </button>
+  
+
         </div>
         {activeTab === "search" && (
           <>
-            <input
-              type="text"
-              placeholder="Ingresa los ASIN separados por comas"
-              value={asinInput}
-              onChange={handleAsinInputChange}
-              className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
-            />
-            <button
-              onClick={handleUpload}
-              className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-            >
-              Cargar
-            </button>
+          <div className="flex items-center justify-center space-x-4">
+  <input
+    type="text"
+    placeholder="Ingresa los ASIN separados por comas"
+    value={asinInput}
+    onChange={handleAsinInputChange}
+    className="flex-grow h-16 px-3 py-2 border rounded-l focus:outline-none focus:ring focus:border-blue-300"
+  />
+  <button
+    onClick={handleUpload}
+    className="bg-blue-500 text-white px-6 py-2 rounded-r hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+  >
+    Buscar
+  </button>
+</div>
+
           </>
         )}
         {activeTab === "import" && (
@@ -164,36 +152,36 @@ export const Dashboard = () => {
             </button>
           </div>
         )}
-        <input
-          type="text"
-          placeholder="Buscar por nombre o ASIN"
-          value={searchTerm}
-          onChange={handleSearch}
-          className="w-full mt-4 px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
-        />
-        <div className="mt-4">
-          <label htmlFor="stockFilter" className="mr-2 text-gray-700">Filtrar por stock:</label>
-          <select
-            id="stockFilter"
-            value={stockFilter}
-            onChange={handleStockFilterChange}
-            className="px-2 py-1 border rounded focus:outline-none focus:ring focus:border-blue-300"
-          >
-            <option value="all">Todos</option>
-            <option value="inStock">Con stock</option>
-            <option value="outOfStock">Sin stock</option>
-          </select>
-        </div>
+<div className="flex items-center border border-white p-4">
+  <input
+    type="text"
+    placeholder="Buscar por nombre o ASIN"
+    value={searchTerm}
+    onChange={handleSearch}
+    className="flex-grow px-3 py-2 border rounded-l focus:outline-none focus:ring focus:border-blue-300"
+  />
+  <select
+    id="stockFilter"
+    value={stockFilter}
+    onChange={handleStockFilterChange}
+    className="flex-grow px-2 py-1 border rounded-r focus:outline-none focus:ring focus:border-blue-300"
+  >
+    <option value="all">Todos</option>
+    <option value="inStock">Con stock</option>
+    <option value="outOfStock">Sin stock</option>
+  </select>
+</div>
+
         {loading ? (
           <p className="mt-4 text-center">Cargando...</p>
         ) : (
           <div className="mt-4">
-            <h3 className="text-lg font-semibold mb-2">Productos subidos:</h3>
+            <h3 className="text-lg font-semibold mb-2 text-white">Productos encontrados:</h3>
             <ul className="space-y-4">
               {filteredProducts.map((product, index) => (
                 <li key={index} className="border p-4 rounded shadow-md">
                   <strong className="block font-semibold text-gray-900">Producto:</strong>
-                  <p className="text-gray-800">{product.name}</p>
+                  <p className="text-gray-80 text-white">{product.name}</p>
                   <strong className="block font-semibold mt-2 text-gray-900">Stock:</strong>
                   <p className="text-green-600">{product.availability_status.includes("In Stock") ? "En stock" : "Sin stock"}</p>
                 </li>
