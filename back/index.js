36eadadc8854873c2bb1896bef53763acf96fca0
Santiago Garcia <102694038+Santy1422@users.noreply.github.com>
2023-08-22 18:44:52 +0200

    update

--- back/index.js
+++ back/index.js
@@ -11,6 +11,7 @@ const {
 const cors = require("cors"); // Import the "cors" middleware
 const mongoose = require("mongoose");
 const Product = require("./routes/product");
+const cron = require("node-cron");
 
 // Initialize app
 const app = express();
@@ -81,6 +82,66 @@ app.post(PRODUCT_DETAILS, async (req, res) => {
     res.json(error);
   }
 });
+
+app.post("/update-products", async (req, res) => {
+  const api_key = "abd4692c8c9b8700b935d228980df52b";
+
+  try {
+    // Eliminar todos los productos existentes
+    await Product.deleteMany();
+
+    // Obtener los IDs de productos almacenados en la base de datos
+    const productsFromDB = await Product.find();
+    const productIds = productsFromDB.map((product) => product.asin);
+
+    // Almacenar los nuevos productos
+    const newProducts = [];
+
+    for (const productId of productIds) {
+      // Hacer la solicitud a la API
+      const response = await request(
+        `${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`
+      );
+
+      const productDetail = JSON.parse(response);
+      console.log(productDetail);
+
+      // Obtener la información de disponibilidad de stock (esto puede variar según la estructura de la respuesta)
+      const isInStock = productDetail.availability_status === "In Stock" || productDetail.pricing !== "";
+
+      // Crear un objeto de producto
+      const newProduct = {
+        asin: productId,
+        name: productDetail.name,
+        hasStock: isInStock,
+      };
+
+      newProducts.push(newProduct);
+    }
+
+    // Insertar los nuevos productos en la base de datos
+    await Product.insertMany(newProducts);
+
+    res.json({ message: "Productos actualizados correctamente." });
+  } catch (error) {
+    res.status(500).json({ error: "Error al actualizar los productos." });
+  }
+});
+
+// ... (resto de tu código)
+
+// Tarea programada para ejecutar la actualización una vez al día
+cron.schedule("0 0 * * *", async () => {
+  console.log("Ejecutando actualización automática de productos...");
+  try {
+    // Llamar a la ruta de actualización de productos aquí
+    const response = await axios.post('https://amazon-scraper-api-production.up.railway.app/update-products');
+    console.log(response.data); // Puedes imprimir la respuesta si lo deseas
+  } catch (error) {
+    console.error("Error en la actualización automática:", error);
+  }
+});
+
 
 app.get('/products', async (req, res) => {
   try {
